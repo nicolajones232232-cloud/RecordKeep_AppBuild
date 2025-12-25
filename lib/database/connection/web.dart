f25582661278db7cadf070b8dd59096f07cb2f3e
NJ <neiljones232@me.com>
2025-12-25 14:20:39 +0000

    Fix database connection for web deployment
    - Update web.dart to use IndexedDB storage instead of deprecated sql.js
    - Remove WASM setup requirements from build script
    - Use WebDatabase.withStorage for better web compatibility
    - Resolves 'Could not access the sql.js javascript library' error

--- lib/database/connection/web.dart
+++ lib/database/connection/web.dart
@@ -2,5 +2,7 @@ import 'package:drift/drift.dart';
 import 'package:drift/web.dart';
 
 Future<QueryExecutor> connect() async {
-  return WebDatabase('recordkeep_db');
+  return WebDatabase.withStorage(
+    DriftWebStorage.indexedDb('recordkeep_db'),
+  );
 }
